c5718e087fb1af0de1529060ee7ef1f0db687f50
Nahuel Pardo <Nahuelpar@hotmail.com.ar>
2023-01-17 12:55:16 -0300

    version-1.0

--- js/main.js
+++ js/main.js
@@ -1,56 +1,122 @@
-let edad = prompt("Cuantos años tenes"); {
-
-    if (edad <= 17) {
-        alert("Eres menor de edad lo siento , volve cuando tengas 18")
-    } else {
-        alert(
-            "Bienvenido ya puedes acceder a nuestros productos"
-        )
-    }
-}
 
 const productos = [
-    { id: 1, nombre: "Fernet Branca", precio: 1500, },
-    { id: 2, nombre: "Gancia", precio: 900, },
-    { id: 3, nombre: "Voodka Absolut", precio: 2500, },
-    { id: 4, nombre: "Aperol", precio: 1500, },
-    { id: 5, nombre: "Whiskey Jack Daniels", precio: 3500, },
-    { id: 6, nombre: "Jagermeister", precio: 2500, },
-    { id: 7, nombre: "Campari", precio: 1200, },
+    {
+        id: "whiskey-jack-daniels",
+        titulo: "Whiskey Jack Daniels",
+        imagen: "./img/whiskeys/01.jpg",
+        categoria: {
+            nombre: "Whiskeys",
+            id: "whiskeys"
+        }
+        , precio: 3500,
+    },
+
+    {
+        id: "jagermeister"
+        , titulo: "Jagermeister",
+        imagen: "./img/whiskeys/02.jpg",
+        categoria: {
+            nombre: "Whiskeys",
+            id: "whiskeys"
+        }
+        , precio: 2500,
+    },
+
+
+
+    {
+        id: "fernet-branca",
+        titulo: "Fernet branca",
+        imagen: "./img/aperitivos/01.jpg",
+        categoria: {
+            nombre: "Aperitivos",
+            id: "aperitivos"
+        }, precio: 1500
+    },
+
+
+
+    {
+        id: "gancia",
+        titulo: "Gancia",
+        imagen: "./img/aperitivos/02.jpg",
+        categoria: {
+            nombre: "Aperitivos",
+            id:  "aperitivos"
+        },
+        precio: 900,
+    },
+
+
+    {
+        id: "aperol",
+        titulo: "Aperol",
+        imagen: "./img/aperitivos/03.jpg",
+        categoria: {
+            nombre: "Aperitivos",
+            id: "aperitivos"
+        }, precio: 1500,
+    },
+
+    {
+        id: "campari",
+        titulo: "Campari",
+        imagen: "./img/aperitivos/04.jpg",
+        categoria: {
+            nombre: "Aperitivos",
+            id: "aperitivos"
+        },
+        precio: 1200,
+    },
+
+    {
+        id: "voodka-absolut",
+        titulo: "Voodka Absolut",
+        imagen: "./img/bebidas-blancas/01.jpg",
+        categoria: {
+            nombre: "Bebidas-blancas",
+            id: "bebidas-blancas"
+        }, precio: 2500,
+    }
+
 ];
 
-function producto(nombre, precio, img) {
-    this.id = productos.length + 1;
-    this.nombre = nombre;
-    this.precio = parseFloat(precio);
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+
+function cargarProductos(productosElegidos) {
+    contenedorProductos.innerHTML= "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = ` 
+        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+        <h3 class="producto-titulo">${producto.titulo}</h3>
+        <p class="producto-precio">$${producto.precio}</p>
+        <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>
+    `;
+        contenedorProductos.append(div)
+    })
+
 }
 
-console.log(productos);
 
-function findProducto(arr, filtro) {
-    const encontrado = arr.find((producto) => {
-        return producto.nombre.includes(filtro.toLowerCase());
-    });
-    return encontrado;
-}
-let buscar = prompt("Ingresa el nombre del producto")
-const produEncontrado = findService(productos, buscar)
-console.log(produEncontrado);
+cargarProductos(productos);
 
-function filtrar(arr, filtro, param) {
-    return arr.filter((el) => {
-        if (param == "precio") {
-            return el[param] <= filtro;
-        } else if (param === "id") {
-            return el[param] == filtro
-        } else {
-            return el[param].includes(filtro)
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
 
-        }
-    });
-}
-const porPrecio = filtrar(productos, 1500, "precio")
-
-console.log(porId);
-const porNombre= filtrar(productos, "Cam", "nombre")
-console.log(porNombre);
+        e.currentTarget.classList.add("active");
+     if(e.currentTarget.id !="todos"){
+        const productosBoton = productos.filter(producto=> producto.categoria.id === e.currentTarget.id);
+        cargarProductos(productosBoton);
+     } else {
+        cargarProductos(productos);
+     }
+    })
+})
